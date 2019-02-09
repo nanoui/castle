ef8a7faf51bf35174bc5b3287d0f3d9bdd1ccbeb
nanoui <34474483+nanoui@users.noreply.github.com>
2019-02-09 17:32:41 +0100

    feat(relais): get the list of hotels restaurant in France from sitemap

--- public/scraping.js
+++ public/scraping.js
@@ -1,19 +1,20 @@
-/*
-    Tutorial : https://codeburst.io/an-introduction-to-web-scraping-with-node-js-1045b55c63f7
-    Purpose : Getting the names and birthdays of US Presidents from Wikipedia
- */
 
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://www.relaischateaux.com/us/destinations/europe/france', (error, response, html) => {
+/*
+   scrap the sitemap in "List of Relais & Chateaux's properties" = the etablissements
+   In this list of etablissement, we are interested in the division "France", listing the french hotels and restaurants
+*/
+
+request('https://www.relaischateaux.com/us/site-map/etablissements', (error, response, html) => {
   if (!error && response.statusCode == 200) {
+    //console.log(html);
 
-    /* get the entire html of the page */
-    const $ = cheerio.load(html);
+    var $ = cheerio.load(html);
 
-    /* get and return in the console the tag SCRIPT from the classe overmapWrap */
-    const destinations = $('.overmapWrap').find('script');
-    console.log(destinations.html());
+    const countryF = $('#countryF').next();
+
+    console.log(countryF.html());
   }
 });
