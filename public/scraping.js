25f5fc7ed94eff9fe4c4f68f38b5d5df8fe179f0
nanoui <34474483+nanoui@users.noreply.github.com>
2019-02-10 11:38:29 +0100

    feat(relais): creating a csv file with the scraped names and links

--- public/scraping.js
+++ public/scraping.js
@@ -2,6 +2,12 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+// write the results in a csv file
+const fs = require('fs');
+const writeStream = fs.createWriteStream('relais.csv');
+// headers in the csv
+writeStream.write(`Name, Link \n`);
+
 /*
    scrap the sitemap in "List of Relais & Chateaux's properties" = the etablissements
    In this list of etablissement, we are interested in the division "France", listing the french hotels and restaurants
@@ -30,7 +36,12 @@ request('https://www.relaischateaux.com/us/site-map/etablissements', (error, res
         .first()
         .attr('href');
 
-      console.log(name, link);
+      //console.log(name, link);
+
+      //write row to csv
+      writeStream.write(`${name}, ${link} \n`);
     });
+
+    console.log('Scraping done...');
   }
 });
